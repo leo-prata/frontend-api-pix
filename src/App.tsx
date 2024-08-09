f9cb45fe8994ca8fbfd01a07260a08f3092c5bc8
leo-prata <leo.prata@estudante.ufjf.br>
2024-08-09 19:17:49 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TicketPix from './payments/TicketPix';
+
+function App() {
+  return (
+    <>
+      <TicketPix />
+    </>
+  );
+}
+
+export default App
---
+++ src/payments/TicketPix.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './styles.module.scss';
+
+const TicketPix = () => {
+    return (
+        <div className={styles.card}>Ticket</div>
+    )
+}
+
+export default TicketPix;
