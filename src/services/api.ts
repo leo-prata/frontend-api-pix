a134bbb26f105f01438b2569b492c38d21ceccf3
leo-prata <leo.prata@estudante.ufjf.br>
2024-11-04 10:33:16 -0300

    update firebase

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function setUpAPI(){
     console.log("Creating Axios instance...");
     const api = axios.create({
-        baseURL: import.meta.env.VITE_API_URL,
+        baseURL: 'http://localhost:3111',
     });
     console.log("Axios instance created:", api);
     return api;
